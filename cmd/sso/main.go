d2e1dd1c18975fc13a3ca38a8f3fd5cdfbe00e28
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-04-12 23:11:58 +0300

    добавлен zap logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,11 +3,42 @@ package main
 import (
     "fmt"
     "go-sso/internal/config"
+    "go.uber.org/zap"
+)
+
+const (
+    envLocal   = "local"
+    envDevelop = "dev"
+    envProd    = "prod"
 )
 
 func main() {
     cfg := config.MustLoad()
 
-    fmt.Println("Config loaded successfully")
-    fmt.Printf("cfg: %#v", cfg)
+    log := setupLogger(cfg.Env)
+
+    log.Infow("SSO service started",
+        "env", cfg.Env,
+        "config", cfg,
+    )
+}
+
+func setupLogger(env string) *zap.SugaredLogger {
+    var log *zap.Logger
+    var err error
+
+    switch env {
+    case envLocal, envDevelop:
+        log, err = zap.NewDevelopment()
+    case envProd:
+        log, err = zap.NewProduction()
+    default:
+        panic("unknown environment: " + env)
+    }
+
+    if err != nil || log == nil {
+        panic(fmt.Sprintf("failed to create logger: %v", err))
+    }
+
+    return log.Sugar()
 }
