5c2fde9c3ccebcd3cb82ae67ca965b96497f35c5
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-04-14 02:09:03 +0300

    исправлена миграция под postgres

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,14 +1,13 @@
-CREATE TABLE IF NOT EXISTS users
-(
-    id        INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id        SERIAL PRIMARY KEY,
     email     TEXT NOT NULL UNIQUE,
-    pass_hash BLOB NOT NULL
+    pass_hash BYTEA NOT NULL
 );
+
 CREATE INDEX IF NOT EXISTS idx_email ON users (email);
 
-CREATE TABLE IF NOT EXISTS apps
-(
-    id     INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS apps (
+    id     SERIAL PRIMARY KEY,
     name   TEXT NOT NULL UNIQUE,
     secret TEXT NOT NULL UNIQUE
 );
