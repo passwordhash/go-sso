89556934f7d08895cdd298b3d5fc037ed507cd46
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-04-25 12:53:55 +0300

    jwt update

--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -1,25 +1,26 @@
 package jwt
 
 import (
-    "github.com/golang-jwt/jwt/v5"
-    "go-sso/internal/domain/models"
-    "time"
+	"github.com/golang-jwt/jwt/v5"
+	"go-sso/internal/domain/models"
+	"time"
 )
 
 // TODO: test
 func NewToken(user models.User, app models.App, duration time.Duration) (string, error) {
-    token := jwt.New(jwt.SigningMethodHS256)
+	token := jwt.New(jwt.SigningMethodHS256)
 
-    claims := token.Claims.(jwt.MapClaims)
-    claims["uid"] = user.ID
-    claims["email"] = user.Email
-    claims["exp"] = time.Now().Add(duration).Unix()
-    claims["app_id"] = app.ID
+	claims := token.Claims.(jwt.MapClaims)
+	claims["uid"] = user.ID
+	claims["email"] = user.Email
+	claims["exp"] = time.Now().Add(duration).Unix()
+	claims["app_id"] = app.ID
 
-    tokenString, err := token.SignedString([]byte(app.Secret))
-    if err != nil {
-        return "", err
-    }
+	// TODO: подумать о безопасном хранении секретов
+	tokenString, err := token.SignedString([]byte(app.Secret))
+	if err != nil {
+		return "", err
+	}
 
-    return tokenString, nil
+	return tokenString, nil
 }
