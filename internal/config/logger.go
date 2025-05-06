100d155ecb79213de6f668f80981dfb6c25df2bc
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-05-06 12:11:12 +0300

    Добавлен в прод среде вывод стека только при error log

--- internal/config/logger.go
+++ internal/config/logger.go
@@ -1,26 +1,29 @@
 package config
 
 import (
-    "fmt"
-    "go.uber.org/zap"
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func SetupLogger(env string) *zap.SugaredLogger {
-    var log *zap.Logger
-    var err error
+	var log *zap.Logger
+	var err error
 
-    switch env {
-    case envLocal, envDevelop:
-        log, err = zap.NewDevelopment()
-    case envProd:
-        log, err = zap.NewProduction()
-    default:
-        panic("unknown environment: " + env)
-    }
+	switch env {
+	case envLocal, envDevelop:
+		log, err = zap.NewDevelopment()
+	case envProd:
+		traceOpt := zap.AddStacktrace(zapcore.ErrorLevel)
+		log, err = zap.NewProduction(traceOpt)
+	default:
+		panic("unknown environment: " + env)
+	}
 
-    if err != nil || log == nil {
-        panic(fmt.Sprintf("failed to create logger: %v", err))
-    }
+	if err != nil || log == nil {
+		panic(fmt.Sprintf("failed to create logger: %v", err))
+	}
 
-    return log.Sugar()
+	return log.Sugar()
 }
